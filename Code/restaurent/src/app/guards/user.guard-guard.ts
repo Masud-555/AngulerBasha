564ba636edd7080c3afc58d60bcd2f4c70535794
Masud-555 <abdullahalmasud011@gmail.com>
2025-07-15 11:44:57 +0600

    15-07-25 guard

--- Code/restaurent/src/app/guards/user.guard-guard.ts
+++ Code/restaurent/src/app/guards/user.guard-guard.ts
@@ -1,5 +1,24 @@
-import { CanActivateFn } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, GuardResult, MaybeAsync, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from '../service/auth-service';
+import { Inject, PLATFORM_ID } from '@angular/core';
+import { Observable } from 'rxjs';
 
-export const userGuardGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+
+export class userGuardGuard implements CanActivate {
+
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { }
+
+
+  canActivate(): boolean | UrlTree | Observable<boolean | UrlTree> {
+    if (this.authService.isAuthenticated() && this.authService.isUser()) {
+      return true;
+    }
+    return this.router.createUrlTree(['/login']);
+  }
+
+}
