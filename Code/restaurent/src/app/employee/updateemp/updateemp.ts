a1ce2f4b462570d21506d57706dd128277bae4bf
Masud-555 <abdullahalmasud011@gmail.com>
2025-07-19 19:37:19 +0600

    add

--- Code/restaurent/src/app/employee/updateemp/updateemp.ts
+++ Code/restaurent/src/app/employee/updateemp/updateemp.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Employee } from '../../../model/emp.model';
+import { EmployeeService } from '../../service/employee.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-updateemp',
@@ -6,6 +9,48 @@ import { Component } from '@angular/core';
   templateUrl: './updateemp.html',
   styleUrl: './updateemp.css'
 })
-export class Updateemp {
+export class Updateemp implements OnInit {
+
+  id: string = '';
+  emp: Employee = new Employee();
+
+  constructor(
+    private empService: EmployeeService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private cdr: ChangeDetectorRef
+
+  ) { }
+
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.loadEmpById();
+  }
+
+
+  loadEmpById(): void {
+    this.empService.getEmpById(this.id).subscribe({
+      next: (res) => {
+        this.emp = res;
+        console.log(res)
+        this.cdr.markForCheck();
+      },
+
+      error: (err) => console.error('Error Fetching Employee:', err)
+    });
+
+  }
+
+  updateEmp(): void {
+    this.empService.updateEmp(this.id, this.emp).subscribe({
+      next: () => this.router.navigate(['/allemp']),
+      error: (err) => console.error('Update failed:', err)
+
+
+    });
+
+
+  }
 
 }
