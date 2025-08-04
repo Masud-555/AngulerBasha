eaa4d51720eed5d4663ad21a6a97b03b326406f3
Masud-555 <abdullahalmasud011@gmail.com>
2025-08-04 18:55:39 +0600

    add

---
+++ Code/Tamplate/src/app/app-module.ts
@@ -0,0 +1,28 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Sidebar } from './layout/sidebar/sidebar';
+import { Navbar } from './layout/navbar/navbar';
+import { Footer } from './layout/footer/footer';
+
+@NgModule({
+  declarations: [
+    App,
+    Sidebar,
+    Navbar,
+    Footer
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
